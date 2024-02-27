79c65ab3dee0e8cd3b31e1c48c6ee1a0579f0079
yutori-engineer <yutori_engineer@yahoo.co.jp>
2024-02-27 22:53:03 +0900

    modified gchart

--- pages/app_page2.py
+++ pages/app_page2.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import streamlit_gchart as gchart
 
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
+cat_data = [
+    ['Phrases'],
+    ['cats are better than dogs'],
+    ['cats eat kibble'],
+    ['cats are better than hamsters'],
+    ['cats are awesome'],
+    ['cats are people too'],
+    ['cats eat mice'],
+    ['cats meowing'],
+    ['cats in the cradle'],
+    ['cats eat mice'],
+    ['cats in the cradle lyrics'],
+    ['cats eat kibble'],
+    ['cats for adoption'],
+    ['cats are family'],
+    ['cats eat mice'],
+    ['cats are better than kittens'],
+    ['cats are evil'],
+    ['cats are weird'],
+    ['cats eat mice']
+]
+    
+gchart.gchart(key="cat_chart", data=cat_data, chartType="WordTree", 
+    width=600, height=400, wordtree={"format": "implicit", "word": "cats"})
